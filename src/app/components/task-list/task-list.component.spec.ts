a019e0b9ef3ec85e282aa24801206f21661ef4da
Nati <natalia@savvyy.ai>
2021-08-20 10:41:49 -0300

    feat: add unit test with angular testing library

--- src/app/components/task-list/task-list.component.spec.ts
+++ src/app/components/task-list/task-list.component.spec.ts
@@ -2,6 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskListComponent } from './task-list.component';
 
+import { render } from '@testing-library/angular';
+
+import { TaskComponent } from '../task/task.component'
+
+//👇 Our story imported here
+import { WithPinnedTasks } from './task-list.component.stories';
+
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
@@ -21,5 +28,25 @@ describe('TaskListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+
+describe('TaskList component', () => {
+  it('renders pinned tasks at the start of the list', async () => {
+    const mockedActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        ...WithPinnedTasks.args,
+        onPinTask: {
+          emit: mockedActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockedActions,
+        } as any,
+      },
+    });
+    const component = tree.fixture.componentInstance;
+    expect(component.tasksInOrder[0].id).toBe('6');
   });
 });
